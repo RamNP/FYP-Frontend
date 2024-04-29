25a64435e1c8799c141d5f240d706f8e9a08a519
ING Skill <ram0004@xavier.eud.np>
2024-04-29 12:52:56 +0545

    Merge remote changes

--- app/src/main/java/com/ram/buspass/features/passVerify/data/VerifyPojo.kt
+++
@@ -1,8 +0,0 @@
-package com.ram.buspass.features.passVerify.data
-
-data class VerifyPojo(
-	val message: String? = null,
-	val is_success: Boolean? = null,
-	val status: Int? = null
-)
-
--- app/src/main/java/com/ram/buspass/helper/resource/remote/api/ApiConstants.kt
+++ app/src/main/java/com/ram/buspass/helper/resource/remote/api/ApiConstants.kt
@@ -1,7 +1,7 @@
 package com.ram.buspass.helper.resource.remote.api
 
 object ApiConstants {
-    const val BASE_URL ="https://cd7b-2400-1a00-b040-77fb-d9bd-66f9-d2bd-de2a.ngrok-free.app/"
+    const val BASE_URL ="https://d607-110-44-125-124.ngrok-free.app/"
 
 
 }
